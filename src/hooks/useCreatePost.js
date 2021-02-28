3dc186707611b13fa521623730dc14f703fe2b2c
Patrick Neulichedl <patrick.neulichedl@andrena.de>
2021-02-28 08:28:09 +0100

    Refactor create and save post.

--- src/hooks/useCreatePost.js
+++ src/hooks/useCreatePost.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import axios from 'axios'
 
+const createPost = (values) =>
+  axios.post('/api/posts', values).then((res) => res.data)
+
 export default function useCreatePost() {
-  const [state, setState] = React.useReducer((_, action) => action, {
-    isIdle: true,
-  })
+  const [state, setState] = React.useState({ isIdle: true })
 
   const mutate = React.useCallback(async (values) => {
     setState({ isLoading: true })
     try {
-      const data = axios.post('/api/posts', values).then((res) => res.data)
+      const data = await createPost(values)
       setState({ isSuccess: true, data })
     } catch (error) {
       setState({ isError: true, error })
--- src/hooks/useSavePost.js
+++ src/hooks/useSavePost.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import axios from 'axios'
 
+const savePost = (values) =>
+  axios.patch(`/api/posts/${values.id}`, values).then((res) => res.data)
+
 export default function useSavePost() {
-  const [state, setState] = React.useReducer((_, action) => action, {
-    isIdle: true,
-  })
+  const [state, setState] = React.useState({ isIdle: true })
 
   const mutate = React.useCallback(async (values) => {
     setState({ isLoading: true })
     try {
-      const data = await axios
-        .patch(`/api/posts/${values.id}`, values)
-        .then((res) => res.data)
+      const data = await savePost(values)
       setState({ isSuccess: true, data })
     } catch (error) {
       setState({ isError: true, error })

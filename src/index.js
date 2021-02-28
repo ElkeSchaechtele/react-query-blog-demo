59811769ea9b7ed7019408ed4ffd187b0acb0a0d
Patrick Neulichedl <patrick.neulichedl@andrena.de>
2021-02-28 09:07:15 +0100

    Add react-query devtools.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 //
-
 import { Wrapper, Main } from './components/styled'
-import Sidebar from './components/Sidebar'
 
+import Sidebar from './components/Sidebar'
 import Admin from './screens/admin'
 import AdminPost from './screens/admin/Post'
 import Blog from './screens/blog'
@@ -18,28 +20,37 @@ function SafeHydrate({ children }) {
   )
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+})
+
 export default function App() {
   return (
     <SafeHydrate>
       <BrowserRouter>
         <Wrapper>
-          <Sidebar />
-          <Main>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <h1>Welcome!</h1>
-                  </>
-                }
-              />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/admin/:postId" element={<AdminPost />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:postId" element={<BlogPost />} />
-            </Routes>
-          </Main>
+          <QueryClientProvider client={queryClient}>
+            <Sidebar />
+            <Main>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <h1>Welcome!</h1>
+                    </>
+                  }
+                />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin/:postId" element={<AdminPost />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/blog/:postId" element={<BlogPost />} />
+              </Routes>
+            </Main>
+            <ReactQueryDevtools />
+          </QueryClientProvider>
         </Wrapper>
       </BrowserRouter>
     </SafeHydrate>
